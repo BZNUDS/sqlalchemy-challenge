04694366fa8546ff2efbee36dd164d9b297dc196
Brian Zdarsky <brian.zdarsky@gmail.com>
2022-03-12 08:33:06 -0600

    WIP

--- app_v01.py
+++ app_v01.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def index():
     return (
         f"Welcome to my SQLAlchemy Climate App Homepage. Avaialble routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
+        f"   /api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start> and /api/v1.0/<start>/<end>"
